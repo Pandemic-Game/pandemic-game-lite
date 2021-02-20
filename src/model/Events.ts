cfcddfc147d176c4a49d2738d37199d98313f7bd
Bruno Felix <bruno.felix@waterdog.mobi>
2021-02-20 17:58:28 +0000

    Added support for typescript

---
+++ src/model/Events.ts
@@ -0,0 +1,18 @@
+import { GameState, Reputation } from './GameState';
+import { Response } from './Response';
+import { Feedback } from './Feedback';
+
+export interface Event {
+    id: string;
+    name: string;
+    description: string;
+    canRun: (gameState: GameState) => boolean;
+    responses: Response[];
+}
+
+export interface CompletedEvent {
+    event: Event;
+    response: Response;
+    feedback: Feedback;
+    reputation: Reputation[];
+}
---
+++ src/model/Feedback.ts
@@ -0,0 +1,6 @@
+export interface Feedback {
+    toResponse: string;
+    fromPublic: string;
+    fromBusiness: string;
+    fromHealthcare: string;
+}
