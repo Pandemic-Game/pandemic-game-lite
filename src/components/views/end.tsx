b89ca3c3094733cbbaeb4ba779759f1e86963bc7
Bruno Felix <bruno.felix@waterdog.mobi>
2021-03-29 19:44:17 +0100

    WiP setting up the component that copies a message to clipboard

--- src/components/views/end.tsx
+++ src/components/views/end.tsx
@@ -1,112 +1,170 @@
-import React, { useEffect } from 'react';
-import { useThemeContext } from '../../ThemeProvider';
-import * as Btn from '../buttons';
-import * as Txt from '../text';
+import React, { useEffect } from "react";
+import { useThemeContext } from "../../ThemeProvider";
+import * as Btn from "../buttons";
+import { EndResultCopyToClipboard } from "../GameResultCopyToClipboard";
+import * as Txt from "../text";
 
-export function Ending(props: { leaderStyle: { ele: JSX.Element, bg: string }, onClick: Function }) {
-    const context = useThemeContext()
+export function Ending(props: {
+  leaderStyle: { ele: JSX.Element; bg: string };
+  onClick: Function;
+}) {
+  const context = useThemeContext();
 
-    useEffect(() => {
-        context.changeBgColorClass(props.leaderStyle.bg)
-    })
+  useEffect(() => {
+    context.changeBgColorClass(props.leaderStyle.bg);
+  });
 
-    return <div className={`p-2 flex flex-col justify-between items-center bg-${props.leaderStyle.bg}`}>
-        <Txt.Subtitle value={'Your leadership style is:'} col='black' />
-        {props.leaderStyle.ele}
-        <Btn.Bouncy
-            col={'white'}
-            bg={'yellow-600'}
-            value={'See what everyone else got'}
-            onClick={() => { props.onClick() }}
-        />
+  return (
+    <div
+      className={`p-2 flex flex-col justify-between items-center bg-${props.leaderStyle.bg}`}
+    >
+      <Txt.Subtitle value={"Your leadership style is:"} col="black" />
+      {props.leaderStyle.ele}
+
+      <EndResultCopyToClipboard text="Share" />
+
+      <Btn.Bouncy
+        col={"white"}
+        bg={"yellow-600"}
+        value={"See what everyone else got"}
+        onClick={() => {
+          props.onClick();
+        }}
+      />
     </div>
+  );
 }
 
-export function ViewEnding(props: { leaderStyle: { ele: JSX.Element, bg: string }, onClick: Function }) {
-    const context = useThemeContext()
+export function ViewEnding(props: {
+  leaderStyle: { ele: JSX.Element; bg: string };
+  onClick: Function;
+}) {
+  const context = useThemeContext();
 
-    useEffect(() => {
-        console.log("Setting bg for: ", props.leaderStyle)
-        context.changeBgColorClass(props.leaderStyle.bg)
-    })
+  useEffect(() => {
+    console.log("Setting bg for: ", props.leaderStyle);
+    context.changeBgColorClass(props.leaderStyle.bg);
+  });
 
-    return <div className={`min-h-full p-2 flex flex-col justify-between items-center bg-${props.leaderStyle.bg}`} >
-        {props.leaderStyle.ele}
-        <Btn.Bouncy value={'Back'} onClick={() => { props.onClick() }} />
+  return (
+    <div
+      className={`min-h-full p-2 flex flex-col justify-between items-center bg-${props.leaderStyle.bg}`}
+    >
+      {props.leaderStyle.ele}
+      <Btn.Bouncy
+        value={"Back"}
+        onClick={() => {
+          props.onClick();
+        }}
+      />
     </div>
+  );
 }
 
 export function AllEndings(props: { onClick: Function }) {
-    const bgColorClass = "bg-yellow-500"
-    const context = useThemeContext()
+  const bgColorClass = "bg-yellow-500";
+  const context = useThemeContext();
 
-    useEffect(() => {
-        context.changeBgColorClass(bgColorClass)
-    })
+  useEffect(() => {
+    context.changeBgColorClass(bgColorClass);
+  });
 
-    return <div className={`min-h-full flex flex-col items-center text-center ${bgColorClass}`}>
-        <div className='p-2 flex flex-col justify-between items-center '>
-            <Txt.Subtitle value='Find out about the other' col='black' />
-            <Txt.Title value='Leadership styles:' col='black' />
+  return (
+    <div
+      className={`min-h-full flex flex-col items-center text-center ${bgColorClass}`}
+    >
+      <div className="p-2 flex flex-col justify-between items-center ">
+        <Txt.Subtitle value="Find out about the other" col="black" />
+        <Txt.Title value="Leadership styles:" col="black" />
+      </div>
+
+      <div className="w-full p-2 flex flex-col justify-center items-center bg-yellow-300">
+        <Txt.SectionTitle value="The reactionaries:" col="gray-700" />
+
+        <div className="max-w-xs p-2 m-2 flex flex-col justify-center items-center">
+          <Btn.Rounded
+            bg="red-500"
+            col="white"
+            value={"Flip-flopper"}
+            onClick={() => {
+              props.onClick("FlipFlopper");
+            }}
+          />
+          <p className="font-bold">1 in 4 people are Flip-Floppers (25%)</p>
+          <Txt.Text
+            value="Flip-floppers focus on giving the public what they want. For example, Boris Johnson (UK)."
+            col="black"
+          />
         </div>
-
-        <div className='w-full p-2 flex flex-col justify-center items-center bg-yellow-300'>
-            <Txt.SectionTitle value='The reactionaries:' col='gray-700' />
-
-            <div className='max-w-xs p-2 m-2 flex flex-col justify-center items-center' >
-                <Btn.Rounded
-                    bg='red-500'
-                    col='white'
-                    value={'Flip-flopper'}
-                    onClick={() => { props.onClick('FlipFlopper') }} />
-                <p className='font-bold'>1 in 4 people are Flip-Floppers (25%)</p>
-                <Txt.Text value='Flip-floppers focus on giving the public what they want. For example, Boris Johnson (UK).' col='black' />
-            </div>
-            <div className='max-w-xs p-2 m-2 flex flex-col justify-center items-center' >
-                <Btn.Rounded
-                    bg='yellow-600'
-                    col='white'
-                    value={'Genghis Cannot'}
-                    onClick={() => { props.onClick('GenghisCannot') }} />
-                <p className='font-bold'>1 in 10 people are Ghenghis Cannots (10%)</p>
-                <Txt.Text value='Once Gheghis Cannots make a choice they do not listen to new evidence (e.g., Donald Trump, USA)' col='black' />
-            </div>
+        <div className="max-w-xs p-2 m-2 flex flex-col justify-center items-center">
+          <Btn.Rounded
+            bg="yellow-600"
+            col="white"
+            value={"Genghis Cannot"}
+            onClick={() => {
+              props.onClick("GenghisCannot");
+            }}
+          />
+          <p className="font-bold">1 in 10 people are Ghenghis Cannots (10%)</p>
+          <Txt.Text
+            value="Once Gheghis Cannots make a choice they do not listen to new evidence (e.g., Donald Trump, USA)"
+            col="black"
+          />
         </div>
+      </div>
 
-        <div className='w-full p-2 flex flex-col justify-center items-center bg-green-400'>
-            <Txt.SectionTitle value='The forward-thinkers:' col='gray-700' />
-            <div className='max-w-xs p-2 m-2 flex flex-col justify-center items-center' >
-                <Btn.Rounded
-                    bg='green-600'
-                    col='white'
-                    value={'COVID-Bane'}
-                    onClick={() => { props.onClick('CovidTerminator') }} />
-                <p className='font-bold'>X in X people are Y</p>
-                <Txt.Text value='COVID eliminators seek to achieve a zero-covid world by implementing strict lockdowns (e.g., Jacinda Ardern, New Zealand)' col='black' />
-            </div>
-            <div className='max-w-xs p-2 m-2 flex flex-col justify-center items-center' >
-                <Btn.Rounded
-                    bg='blue-500'
-                    col='white'
-                    value={'Economic Savior'}
-                    onClick={() => { props.onClick('BusinessGuru') }} />
-                <p className='font-bold'>X in X people are Y</p>
-                <Txt.Text value='Economy savers deal first with COVID so that businesses can open up (e.g., Chung Seye-Kyun, South Korea)' col='black' />
-            </div>
+      <div className="w-full p-2 flex flex-col justify-center items-center bg-green-400">
+        <Txt.SectionTitle value="The forward-thinkers:" col="gray-700" />
+        <div className="max-w-xs p-2 m-2 flex flex-col justify-center items-center">
+          <Btn.Rounded
+            bg="green-600"
+            col="white"
+            value={"COVID-Bane"}
+            onClick={() => {
+              props.onClick("CovidTerminator");
+            }}
+          />
+          <p className="font-bold">X in X people are Y</p>
+          <Txt.Text
+            value="COVID eliminators seek to achieve a zero-covid world by implementing strict lockdowns (e.g., Jacinda Ardern, New Zealand)"
+            col="black"
+          />
         </div>
+        <div className="max-w-xs p-2 m-2 flex flex-col justify-center items-center">
+          <Btn.Rounded
+            bg="blue-500"
+            col="white"
+            value={"Economic Savior"}
+            onClick={() => {
+              props.onClick("BusinessGuru");
+            }}
+          />
+          <p className="font-bold">X in X people are Y</p>
+          <Txt.Text
+            value="Economy savers deal first with COVID so that businesses can open up (e.g., Chung Seye-Kyun, South Korea)"
+            col="black"
+          />
+        </div>
+      </div>
 
-        <div className='max-w-xs p-2 m-2 flex flex-col justify-center items-center'>
-            <Txt.Subtitle value='How do we calculate these?' col='black' />
-            <Txt.Text value='We showed you a scenario where you could have responded in different ways. Your responses have consequences later down the line, like a story tree.' col='black' />
-        </div>
+      <div className="max-w-xs p-2 m-2 flex flex-col justify-center items-center">
+        <Txt.Subtitle value="How do we calculate these?" col="black" />
+        <Txt.Text
+          value="We showed you a scenario where you could have responded in different ways. Your responses have consequences later down the line, like a story tree."
+          col="black"
+        />
+      </div>
 
-        <div style={{ marginTop: "auto" }}>
-            <Btn.Bouncy
-                value={'Play again'}
-                bg={'purple-900'}
-                col={'white'}
-                onClick={() => { window.location.reload() }}
-            />
-        </div>
+      <div style={{ marginTop: "auto" }}>
+        <Btn.Bouncy
+          value={"Play again"}
+          bg={"purple-900"}
+          col={"white"}
+          onClick={() => {
+            window.location.reload();
+          }}
+        />
+      </div>
     </div>
+  );
 }
--- src/components/views/start.tsx
+++ src/components/views/start.tsx
@@ -1,99 +1,117 @@
-import React, { useEffect } from 'react';
-import EndCoronaVirusLogo from '../../assets/PNG/ecvlogo.png';
-import { useThemeContext } from '../../ThemeProvider';
-import * as Btn from '../buttons';
-import * as Txt from '../text';
-import { WarningSign } from '../warning-sign';
+import React, { useEffect } from "react";
+import EndCoronaVirusLogo from "../../assets/PNG/ecvlogo.png";
+import { useThemeContext } from "../../ThemeProvider";
+import * as Btn from "../buttons";
+import * as Txt from "../text";
+import { WarningSign } from "../warning-sign";
 
 export function Splash(props: { onClick: Function }) {
-    const bgColorClass = "bg-yellow-400"
-    const context = useThemeContext()
+  const bgColorClass = "bg-yellow-400";
+  const context = useThemeContext();
 
-    useEffect(() => {
-        context.changeBgColorClass(bgColorClass)
-    })
+  useEffect(() => {
+    context.changeBgColorClass(bgColorClass);
+  });
 
-    return (
-        <div className={`min-h-full p-2 flex flex-col items-center ${bgColorClass}`}>
-            <div className="m-2">
-                <a
-                    href='https://www.endcoronavirus.org'
-                    rel='noreferrer'
-                    target='_blank'
-                    className='text-blue-900'
-                >
-                    <img
-                        src={EndCoronaVirusLogo}
-                        alt='EndCoronaVirusLogo'
-                        className='m-2 w-auto h-14 rounded-lg'>
-                    </img>
-                </a>
-            </div>
+  return (
+    <div
+      className={`min-h-full p-2 flex flex-col items-center ${bgColorClass}`}
+    >
+      <div className="m-2">
+        <a
+          href="https://www.endcoronavirus.org"
+          rel="noreferrer"
+          target="_blank"
+          className="text-blue-900"
+        >
+          <img
+            src={EndCoronaVirusLogo}
+            alt="EndCoronaVirusLogo"
+            className="m-2 w-auto h-14 rounded-lg"
+          ></img>
+        </a>
+      </div>
 
-            <div className="m-2">
-                <Txt.BigTitle value={'Pandemic Game'} />
-            </div>
+      <div className="m-2">
+        <Txt.BigTitle value={"Pandemic Game"} />
+      </div>
 
-
-            <div className='flex m-2 flex-col justify-center items-center'>
-                <Txt.Subtitle value={'You are a World Leader.'} col='black' />
-                <p className='m-2 text-center font-sans font-medium'>
-                    Your job is to navigate the first few months of 2021.
-                    The decisions you make will polarize different parts of society, from the public to businesses and healthcare.
-                </p>
-                <Txt.Subtitle value={'What type of leader are you?'} col='black' />
-            </div>
-            <div style={{ marginTop: "auto" }}>
-                <Btn.Bouncy
-                    value='Play the game'
-                    bg='purple-900'
-                    col='white'
-                    onClick={props.onClick}
-                />
-            </div>
-        </div>
-    );
+      <div className="flex m-2 flex-col justify-center items-center">
+        <Txt.Subtitle value={"You are a World Leader."} col="black" />
+        <p className="m-2 text-center font-sans font-medium">
+          Your job is to navigate the first few months of 2021. The decisions
+          you make will polarize different parts of society, from the public to
+          businesses and healthcare.
+        </p>
+        <Txt.Subtitle value={"What type of leader are you?"} col="black" />
+      </div>
+      <div style={{ marginTop: "auto" }}>
+        <Btn.Bouncy
+          value="Play the game"
+          bg="purple-900"
+          col="white"
+          onClick={props.onClick}
+        />
+      </div>
+    </div>
+  );
 }
 
-
-export function Introduction(props: { onClickContinue: Function, onClickSource: Function }) {
-    return (
-        <div className='min-h-full p-2 flex flex-col items-center bg-yellow-400'>
-            <div className='flex flex-col justify-between items-center'>
-                <Txt.Subtitle value={'El presidente!'} col='black' />
-                <Txt.Title value='A situation requires your attention!' col='gray-900' />
-            </div>
-            <div className='animate__animated animate__fadeIn  m-2 p-2 flex flex-row items-start'>
-                <div className='m-2 p-2 flex flex-row'>
-                    <WarningSign />
-                </div>
-                <Txt.Paragraph value={`   
+export function Introduction(props: {
+  onClickContinue: Function;
+  onClickSource: Function;
+}) {
+  return (
+    <div className="min-h-full p-2 flex flex-col items-center bg-yellow-400">
+      <div className="flex flex-col justify-between items-center">
+        <Txt.Subtitle value={"El presidente!"} col="black" />
+        <Txt.Title
+          value="A situation requires your attention!"
+          col="gray-900"
+        />
+      </div>
+      <div className="animate__animated animate__fadeIn  m-2 p-2 flex flex-row items-start">
+        <div className="m-2 p-2 flex flex-row">
+          <WarningSign />
+        </div>
+        <Txt.Paragraph
+          value={`   
                     Vaccines for COVID-19 have been developed but they will take a while to work.
                     
                     The way you respond to the pandemic in the next few months will have consequences.
                     
                     You can probably do this in the next 5 minutes.
-                `} col='black' />
-            </div>
-            <div className='flex flex-col justify-center items-center'>
-                <Btn.ViewSource onClick={props.onClickSource} sourceDetails={
-                    {
-                        sourceName: 'Interactive data model',
-                        link: 'http://pandemic-game-prod.s3-website.us-east-2.amazonaws.com',
-                        description: 'These screens will show you our sources. For example, our data model is based on US and UK data, including extensive polls of the UK population as well as COVID and economic data from UK and US sources.'
-                    }
-                } />
-                <Txt.Subtitle value={'This game is based on real data. Look out for question mark icons and press them to inspect our evidence'} col='black' />
-            </div>
+                `}
+          col="black"
+        />
+      </div>
+      <div className="flex flex-col justify-center items-center">
+        <Btn.ViewSource
+          onClick={props.onClickSource}
+          sourceDetails={{
+            sourceName: "Interactive data model",
+            link:
+              "http://pandemic-game-prod.s3-website.us-east-2.amazonaws.com",
+            description:
+              "These screens will show you our sources. For example, our data model is based on US and UK data, including extensive polls of the UK population as well as COVID and economic data from UK and US sources.",
+          }}
+        />
+        <Txt.Subtitle
+          value={
+            "This game is based on real data. Look out for question mark icons and press them to inspect our evidence"
+          }
+          col="black"
+        />
+      </div>
 
-            <div style={{ marginTop: "auto" }}>
-                <Btn.Bouncy
-                    value='Start'
-                    bg='purple-900'
-                    col='white'
-                    onClick={props.onClickContinue}
-                />
-            </div>
-        </div >
-    );
+      <div style={{ marginTop: "auto" }}>
+        <Btn.Bouncy
+          value="Start"
+          bg="purple-900"
+          col="white"
+          onClick={props.onClickContinue}
+        />
+      </div>
+    </div>
+  );
 }
